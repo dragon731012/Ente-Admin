f959347b31f3ff4a1cc0c6627bf89be300e301dc
dragon731012 <dragon73101@gmail.com>
2026-06-06 12:13:09 -0500

    styling

--- login.php
+++ login.php
@@ -15,14 +15,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
-<form method="POST" action="login.php">
-    <div class="form-group">
-        <label>email</label>
-        <input type="text" name="user" required autocomplete="off">
+<div class="title">Ente Admin Panel</div>
+
+<form method="POST" action="login.php" class="login-cont">
+    <div class="login-inner-cont">
+        <label class="login-label">email</label>
+        <input type="text" name="user" class="login-input" placeholder="Username" required autocomplete="off">
     </div>
-    <div class="form-group">
-        <label>pw</label>
-        <input type="password" name="password" required autocomplete="off">
+    <div class="login-inner-cont">
+        <label class="login-label">pw</label>
+        <input type="password" name="password" class="login-input" placeholder="Pasword" required autocomplete="off">
     </div>
-    <button type="submit">login</button>
+    <button type="submit" class="login-button">Login</button>
 </form>
+
+<link rel="stylesheet" href="style.css">
